9484ca63d4e2af2bfd759b5a72e5c3b1ef6a270f
agoujet <agoujet@parigo.net>
2014-11-04 23:05:16 +0100

    Update configure-C-Z-all-node.sh

--- configure-C-Z-all-node.sh
+++ configure-C-Z-all-node.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pssh -i -h hostpssh /opt/mapr/server/configure.sh -C ip-10-1-3-118,ip-10-1-3-111 -Z ip-10-1-3-111,ip-10-1-3-112,ip-10-1-3-129 -N MapR-Cluster
+clush -a /opt/mapr/server/configure.sh -C ip-10-1-3-118,ip-10-1-3-111 -Z ip-10-1-3-111,ip-10-1-3-112,ip-10-1-3-129 -N MapR-Cluster
 
