bb39fb29dfbe9574ef01ca97f1cd52a9b956771f
agoujet <agoujet@parigo.net>
2014-11-05 00:20:28 +0100

    Delete fix-zookeeper.sh

--- fix-zookeeper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-pssh -i -h hostpssh  cp /opt/mapr/zookeeper/zookeeper-3.3.6/conf.new/* /opt/mapr/zookeeper/zookeeper-3.3.6/conf
-pssh -i -h hostpssh touch /opt/mapr/zkdata/zookeeper_server.pid
-pssh -i -h hostpssh chown -R mapr /opt/mapr/zkdata
-pssh -i -h hostpssh chgrp -R mapr /opt/mapr/zookeeper/
-
