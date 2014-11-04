2bec1110c4af5d66f957e45baaf6f59971fc20a5
agoujet <agoujet@parigo.net>
2014-11-05 00:21:51 +0100

    Update list-volumes.sh

--- list-volumes.sh
+++ list-volumes.sh
@@ -1,2 +1,2 @@
-maprcli volume list -columns mountdir,volumename,rackpath,totalused,logicalUsed,mounted,needsGfsck | grep vol1
+maprcli volume list -columns mountdir,volumename,rackpath,totalused,logicalUsed,mounted,needsGfsck
 
